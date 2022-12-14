60bf0d5212406568d5df0c4d172780deabc88f17
Cosmin Petre <cpetre@pa.uc3m.es>
2022-12-14 23:40:07 +0100

    implemented RTEMS part 1 tasks need to do scheduler now

--- music_rtems/music_rtems_part1.c
+++ music_rtems/music_rtems_part1.c
@@ -21,32 +21,38 @@
 /**********************************************************
  *  CONSTANTS
  *********************************************************/
+#define SLAVE_ADDR 0x8
 #define NSEC_PER_SEC 1000000000UL
 
 #define DEV_NAME "/dev/com1"
-#define FILE_NAME "/let_it_be_1bit.raw"
+#define AUDIOFILE_NAME "/let_it_be_1bit.raw"
 
-#define PERIOD_TASK_SEC    0            /* Period of Task */
-#define PERIOD_TASK_NSEC  512000000    /* Period of Task */
-#define SEND_SIZE 256                /* BYTES */
+#define PERIOD_TASK_PLAYBACK_SEC                0           /* Period of Playback Task */
+#define PERIOD_TASK_PLAYBACK_NSEC               512000000   /* Period of Playback Task */
+#define PERIOD_TASK_READ_COMMANDS_SEC           1           /* Period of Read Commands Task */
+#define PERIOD_TASK_READ_COMMANDS_NSEC          0           /* Period of Read Commands Task */
+#define PERIOD_TASK_SHOW_PLAYBACK_STATE_SEC     2           /* Period of Show Playback State Task */
+#define PERIOD_TASK_SHOW_PLAYBACK_STATE_NSEC    500000000   /* Period of Show Playback State Task */
+#define SEND_SIZE                               256         /* BYTES */
 
+// audiofile tarball stuff
 #define TARFILE_START _binary_tarfile_start
 #define TARFILE_SIZE _binary_tarfile_size
 
-#define SLAVE_ADDR 0x8
+typedef enum{paused, playing} state_t;
+
 
 /**********************************************************
  *  GLOBALS
  *********************************************************/
+// audiofile tarball stuff
 extern int _binary_tarfile_start;
 extern int _binary_tarfile_size;
+// playback state: governs whether the audiofile is read or not
+state_t playback_state = playing;
 
-/**********************************************************
- * Function: diffTime
- *********************************************************/
-void diffTime(struct timespec end,
-              struct timespec start,
-              struct timespec *diff)
+
+void diffTime(struct timespec end, struct timespec start, struct timespec *diff)
 {
     if (end.tv_nsec < start.tv_nsec) {
         diff->tv_nsec = NSEC_PER_SEC - start.tv_nsec + end.tv_nsec;
@@ -57,12 +63,8 @@ void diffTime(struct timespec end,
     }
 }
 
-/**********************************************************
- * Function: addTime
- *********************************************************/
-void addTime(struct timespec end,
-              struct timespec start,
-              struct timespec *add)
+
+void addTime(struct timespec end, struct timespec start, struct timespec *add)
 {
     unsigned long aux;
     aux = start.tv_nsec + end.tv_nsec;
@@ -71,11 +73,8 @@ void addTime(struct timespec end,
     add->tv_nsec = aux % NSEC_PER_SEC;
 }
 
-/**********************************************************
- * Function: compTime
- *********************************************************/
-int compTime(struct timespec t1,
-              struct timespec t2)
+
+int compTime(struct timespec t1, struct timespec t2)
 {
     if (t1.tv_sec == t2.tv_sec) {
         if (t1.tv_nsec == t2.tv_nsec) {
@@ -94,26 +93,128 @@ int compTime(struct timespec t1,
 }
 
 
-/*****************************************************************************
- * Function: Init()
- *****************************************************************************/
+void task_playback(int fd_serie, int fd_file, unsigned char *buf)
+{
+    struct timespec start, end, diff, cycle;
+    int ret;
+
+    cycle.tv_sec = PERIOD_TASK_PLAYBACK_SEC;
+    cycle.tv_nsec = PERIOD_TASK_PLAYBACK_NSEC;
+
+    clock_gettime(CLOCK_REALTIME, &start);
+    while (1) {
+        // read from music file
+        ret = read(fd_file, buf, SEND_SIZE);
+        if (ret < 0) {
+            printf("read: error reading file\n");
+            exit(-1);
+        }
+        
+        // write on the serial/I2C port
+        ret = write(fd_serie, buf, SEND_SIZE);
+        if (ret < 0) {
+            printf("write: error writting serial\n");
+            exit(-1);
+        }
+
+        // get end time, calculate lapso and sleep
+        clock_gettime(CLOCK_REALTIME, &end);
+        diffTime(end, start, &diff);
+        if (0 >= compTime(cycle, diff)) {
+            printf("ERROR: lasted long than the cycle\n");
+            exit(-1);
+        }
+        diffTime(cycle, diff, &diff);
+        nanosleep(&diff, NULL);
+        addTime(start, cycle, &start);
+    }
+}
+
+
+void task_read_commands()
+{
+    struct timespec start, end, diff, cycle;
+    char keyboard_input = '1';
+
+    cycle.tv_sec = PERIOD_TASK_READ_COMMANDS_SEC;
+    cycle.tv_nsec = PERIOD_TASK_READ_COMMANDS_NSEC;
+    clock_gettime(CLOCK_REALTIME, &start);
+
+    while (1) {
+        switch (keyboard_input) {
+            case '0':
+                playback_state = 0;
+                break;
+            case '1':
+                playback_state = 1;
+                break;
+        }
+
+        //TODO: remove temporary this stuff
+        switch (playback_state) {
+            case playing:
+                printf("Reproduction resumed\n"); fflush(stdout);
+                break;
+            case paused:
+                printf("Reproduction paused\n"); fflush(stdout);
+                break;
+        }
+        
+        while ( scanf("%c", &keyboard_input) <= 0 );
+        clock_gettime(CLOCK_REALTIME, &end);
+
+        diffTime(end, start, &diff);
+        while( compTime(diff, cycle) > 0 )
+            diffTime(diff, cycle, &diff);
+
+        nanosleep(&diff, NULL);
+        addTime(start, cycle, &start);
+    }
+}
+
+
+void task_show_playback_state()
+{
+    struct timespec start, end, diff, cycle;
+
+    cycle.tv_sec = PERIOD_TASK_SHOW_PLAYBACK_STATE_SEC;
+    cycle.tv_nsec = PERIOD_TASK_SHOW_PLAYBACK_STATE_NSEC;
+    clock_gettime(CLOCK_REALTIME, &start);
+
+    while (1) {
+        switch (playback_state) {
+            case playing:
+                printf("Reproduction resumed\n"); fflush(stdout);
+                break;
+            case paused:
+                printf("Reproduction paused\n"); fflush(stdout);
+                break;
+        }
+
+        clock_gettime(CLOCK_REALTIME, &end);
+        diffTime(end, start, &diff);
+
+        nanosleep(&diff, NULL);
+        addTime(start, cycle, &start);
+    }
+}
+
+
 rtems_task Init (rtems_task_argument ignored)
 {
-    struct timespec start,end,diff,cycle;
+    struct timespec start, end, diff, cycle;
     unsigned char buf[SEND_SIZE];
     int fd_file = -1;
     int fd_serie = -1;
     int ret = 0;
 
     printf("Populating Root file system from TAR file.\n");
-    Untar_FromMemory((unsigned char *)(&TARFILE_START),
-                     (unsigned long)&TARFILE_SIZE);
+    Untar_FromMemory((unsigned char *) (&TARFILE_START), (unsigned long) &TARFILE_SIZE);
 
-    rtems_shell_init("SHLL", RTEMS_MINIMUM_STACK_SIZE * 4,
-                     100, "/dev/foobar", false, true, NULL);
+    rtems_shell_init("SHLL", RTEMS_MINIMUM_STACK_SIZE * 4, 100, "/dev/foobar", false, true, NULL);
 
     /* Open serial port */
-    printf("open serial device %s \n",DEV_NAME);
+    printf("open serial device %s \n", DEV_NAME);
     fd_serie = open (DEV_NAME, O_RDWR);
     if (fd_serie < 0) {
         printf("open: error opening serial %s\n", DEV_NAME);
@@ -121,7 +222,7 @@ rtems_task Init (rtems_task_argument ignored)
     }
 
     struct termios portSettings;
-    speed_t speed=B115200;
+    speed_t speed = B115200;
 
     tcgetattr(fd_serie, &portSettings);
     cfsetispeed(&portSettings, speed);
@@ -130,44 +231,15 @@ rtems_task Init (rtems_task_argument ignored)
     tcsetattr(fd_serie, TCSANOW, &portSettings);
 
     /* Open music file */
-    printf("open file %s begin\n",FILE_NAME);
-    fd_file = open (FILE_NAME, O_RDWR);
+    printf("open file %s begin\n", AUDIOFILE_NAME);
+    fd_file = open (AUDIOFILE_NAME, O_RDWR);
     if (fd_file < 0) {
         perror("open: error opening file \n");
         exit(-1);
     }
 
-    // loading cycle time
-    cycle.tv_sec=PERIOD_TASK_SEC;
-    cycle.tv_nsec=PERIOD_TASK_NSEC;
-
-    clock_gettime(CLOCK_REALTIME,&start);
-    while (1) {
-        // read from music file
-        ret=read(fd_file,buf,SEND_SIZE);
-        if (ret < 0) {
-            printf("read: error reading file\n");
-            exit(-1);
-        }
-        
-        // write on the serial/I2C port
-        ret=write(fd_serie,buf,SEND_SIZE);
-        if (ret < 0) {
-            printf("write: error writting serial\n");
-            exit(-1);
-        }
-
-        // get end time, calculate lapso and sleep
-        clock_gettime(CLOCK_REALTIME,&end);
-        diffTime(end,start,&diff);
-        if (0 >= compTime(cycle,diff)) {
-            printf("ERROR: lasted long than the cycle\n");
-            exit(-1);
-        }
-        diffTime(cycle,diff,&diff);
-        nanosleep(&diff,NULL);
-        addTime(start,cycle,&start);
-    }
+    task_read_commands();
+    // task_playback(fd_serie, fd_file, buf);
     exit(0);
 
 } /* End of Init() */
@@ -181,4 +253,3 @@ rtems_task Init (rtems_task_argument ignored)
 #define CONFIGURE_UNLIMITED_OBJECTS
 #define CONFIGURE_INIT
 #include <rtems/confdefs.h>
-
