9f22bddb0ef81d9f31054c118ae7fb800d445f47
JORGE MATARIN SERRANO <100429088@alumnos.uc3m.es>
2022-12-19 17:50:36 +0100

    Part 2 only sounds piano

--- music_arduino/music_arduino_part2/music_arduino_part2.ino
+++ music_arduino/music_arduino_part2/music_arduino_part2.ino
@@ -1,7 +1,7 @@
 /**********************************************************
  *  INCLUDES
  *********************************************************/
-#include "let_it_be_1bit.h"
+#include "let_it_be.h"
 
 /**********************************************************
  *  CONSTANTS
@@ -9,9 +9,21 @@
 // COMMENT THIS LINE TO EXECUTE WITH THE PC
 #define TEST_MODE 1
 
-#define SAMPLE_TIME 250 
+#define SAMPLE_TIME 3999 
 #define SOUND_PIN  11
 #define BUF_SIZE 256
+#define PUSH_BUTTON 7
+#define LED 13
+
+#define TIME_TASK(TASK) \
+    time_exec_begin = micros(); \
+    (TASK); \
+    time_exec_end = micros(); \
+    elapsed = time_exec_end - time_exec_begin;
+
+typedef enum{muted, playing} state_t;
+state_t playback_state = playing;
+int old_value_button = 0;
 
 /**********************************************************
  *  GLOBALS
@@ -21,26 +33,44 @@ unsigned long timeOrig;
 
 /**********************************************************
  * Function: play_bit
+ Worst Compute Time: 28 μs
  *********************************************************/
-void play_bit() 
+void play_byte() 
 {
-  static int bitwise = 1;
   static unsigned char data = 0;
   static int music_count = 0;
 
-    bitwise = (bitwise * 2);
-    if (bitwise > 128) {
-       bitwise = 1;
-       #ifdef TEST_MODE 
-          data = pgm_read_byte_near(music + music_count);
-          music_count = (music_count + 1) % MUSIC_LEN;
-       #else 
-          if (Serial.available()>1) {
-             data = Serial.read();
-          }
-       #endif
+    #ifdef TEST_MODE 
+      data = pgm_read_byte_near(music + music_count);
+      music_count = (music_count + 1) % MUSIC_LEN;
+    #else 
+      if (Serial.available()>1) {
+          data = Serial.read();
+      }
+    #endif
+    
+    switch (playback_state) {
+        case playing:
+            OCR2A = data;
+            break;
+        case muted:
+            OCR2A=0;
+            break;
     }
-    digitalWrite(SOUND_PIN, (data & bitwise) );
+}
+
+/**********************************************************
+ * Function: read_button_task
+ Worst Compute Time: 12 μs
+ *********************************************************/
+void read_button_task()
+{
+    int value = digitalRead(PUSH_BUTTON);
+    if ( (old_value_button == 0) && (value == 1) ) {
+        playback_state = (state_t) !playback_state;
+        digitalWrite(LED, !playback_state);      
+    }
+    old_value_button = value;
 }
 
 /**********************************************************
@@ -53,8 +83,25 @@ void setup ()
 
     pinMode(SOUND_PIN, OUTPUT);
     memset (buffer, 0, BUF_SIZE);
-    timeOrig = micros();    
+    pinMode(LED, OUTPUT);
+    pinMode(PUSH_BUTTON, INPUT);
+
+    TCCR1A = _BV(COM1A0);
+    TCCR1B = _BV(WGM12) | _BV(CS10);
+    TIMSK1 = _BV(OCIE1A);
+
+    TCCR2A = _BV(WGM21) | _BV(WGM20) | _BV(COM2A1);
+    TCCR2B = _BV(CS20);
+    
+    OCR1A=SAMPLE_TIME;
+    OCR1B=0;
+    OCR2A=0;
 }
+
+ISR(TIMER1_COMPA_vect){
+    play_byte();  
+}
+
 
 /**********************************************************
  * Function: loop
@@ -62,9 +109,10 @@ void setup ()
 void loop ()
 {
     unsigned long timeDiff;
-
-    play_bit();
-    timeDiff = SAMPLE_TIME - (micros() - timeOrig);
-    timeOrig = timeOrig + SAMPLE_TIME;
-    delayMicroseconds(timeDiff);
+    noInterrupts();
+    read_button_task();
+    interrupts();    
+    //timeDiff = SAMPLE_TIME - (micros() - timeOrig);
+    //timeOrig = timeOrig + SAMPLE_TIME;
+    //delayMicroseconds(timeDiff);
 }
